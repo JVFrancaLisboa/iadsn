1af3cd2abf80569e6873076e15bea854e485086e
SyntaxxSeeker <logiccoder88@gmail.com>
2024-11-29 10:59:26 -0300

    Initial Project

---
+++ iadsn/src/main/java/com/iadsn/IadsnApplication.java
@@ -0,0 +1,13 @@
+package com.iadsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IadsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IadsnApplication.class, args);
+	}
+
+}
---
+++ iadsn/src/test/java/com/iadsn/IadsnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iadsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IadsnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
