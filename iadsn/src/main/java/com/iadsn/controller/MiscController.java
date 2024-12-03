21c177033437dab6c182e0d0510cbdee40c1f1cb
SyntaxxSeeker <logiccoder88@gmail.com>
2024-12-02 21:04:20 -0300

    Implementando disgn de navbar

---
+++ iadsn/src/main/java/com/iadsn/controller/MiscController.java
@@ -0,0 +1,13 @@
+package com.iadsn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MiscController {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "home";
+    }
+}
