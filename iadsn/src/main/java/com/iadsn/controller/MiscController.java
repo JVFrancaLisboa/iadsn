90c4879d5ed0d9cebe7a917d7c2eb37c41990b66
SyntaxxSeeker <logiccoder88@gmail.com>
2024-12-03 21:07:42 -0300

    Iniciando formulário de cadastro de membros

--- iadsn/src/main/java/com/iadsn/controller/MiscController.java
+++ iadsn/src/main/java/com/iadsn/controller/MiscController.java
@@ -10,4 +10,19 @@ public class MiscController {
     public String getHome(){
         return "home";
     }
+
+    @GetMapping("/about")
+    public String getAbout(){
+        return "about";
+    }
+
+    @GetMapping("/movimentacoes")
+    public String getMovs(){
+        return "movs";
+    }
+
+    @GetMapping("/membros")
+    public String getMembros(){
+        return "membros";
+    }
 }
