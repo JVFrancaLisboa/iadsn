eb798a29bc143e79e96b02b8e18ffbaa334c48e0
SyntaxxSeeker <logiccoder88@gmail.com>
2024-12-06 01:42:01 -0300

    Tela de movimentações navegando implementada a div dinâmica

--- iadsn/src/main/resources/static/js/ajaxNavigation.js
+++ iadsn/src/main/resources/static/js/ajaxNavigation.js
@@ -1,8 +1,30 @@
 $(document).ready(function () {
     // Adiciona o evento de clique para o link "Membros"
-    $("#ajax-link").click(function (event) {
+    $("#ajax-link-2").click(function (event) {
         event.preventDefault(); // Impede o comportamento padrão do link
+        const url = $(this).data("url"); // Obtém a URL do atributo data-url
 
+        // Carrega o conteúdo via AJAX
+        $.ajax({
+            url: url,
+            type: "GET",
+            beforeSend: function () {
+                // Exibe uma mensagem de carregamento
+                $("#dynamic-content").html("<div class='text-center'><p>Carregando...</p></div>");
+            },
+            success: function (response) {
+                // Insere o conteúdo carregado na div
+                $("#dynamic-content").html(response);
+            },
+            error: function () {
+                // Exibe uma mensagem de erro em caso de falha
+                $("#dynamic-content").html("<div class='text-center text-danger'><p>Erro ao carregar o conteúdo.</p></div>");
+            }
+        });
+    });
+    // Adiciona o evento de clique para o link "Movimentações"
+    $("#ajax-link-1").click(function (event) {
+        event.preventDefault(); // Impede o comportamento padrão do link
         const url = $(this).data("url"); // Obtém a URL do atributo data-url
 
         // Carrega o conteúdo via AJAX
