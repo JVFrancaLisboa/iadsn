38c83ff8ee95138ea08942709872e997cef5a34d
SyntaxxSeeker <logiccoder88@gmail.com>
2024-12-31 16:02:46 -0300

    Implementation of the OfertaService class

---
+++ iadsn/src/main/java/com/iadsn/services/OfertaService.java
@@ -0,0 +1,25 @@
+package com.iadsn.services;
+
+import com.iadsn.entities.OfertaEntity;
+import com.iadsn.repository.OfertaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OfertaService {
+
+    @Autowired
+    OfertaRepository ofertaRepository;
+
+    public OfertaEntity getOfertaId(Long id){
+        return ofertaRepository.findById(id).orElse(null);
+    }
+
+    public OfertaEntity criarOferta(OfertaEntity oferta){
+        oferta.setId(null);
+        return ofertaRepository.save(oferta);
+    }
+
+    public void deletarOferta(Long id){
+        OfertaEntity oferta = getOfertaId(id);
+        ofertaRepository.deleteById(oferta.getId());
+    }
+}
