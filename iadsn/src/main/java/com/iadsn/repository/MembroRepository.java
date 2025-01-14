cfc7cde72bb846f2ce8f705b45f4a3371e1fdb70
SyntaxxSeeker <logiccoder88@gmail.com>
2025-01-14 19:18:45 -0300

    Implemented the query to the database for archived members

--- iadsn/src/main/java/com/iadsn/repository/MembroRepository.java
+++ iadsn/src/main/java/com/iadsn/repository/MembroRepository.java
@@ -2,8 +2,14 @@ package com.iadsn.repository;
 
 import com.iadsn.entities.MembroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MembroRepository extends JpaRepository<MembroEntity, Long> {
+
+    @Query("SELECT m FROM MembroEntity m WHERE m.arquivado = true")
+    List<MembroEntity> findMembrosArquivados();
 }
