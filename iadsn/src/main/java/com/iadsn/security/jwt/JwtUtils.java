de958f6bcfec87a0290fd0a73f16fdb83924a206
SyntaxxSeeker <logiccoder88@gmail.com>
2025-02-13 20:24:13 -0300

    created class ImplDadosUsuario

--- iadsn/src/main/java/com/iadsn/security/jwt/JwtUtils.java
+++ iadsn/src/main/java/com/iadsn/security/jwt/JwtUtils.java
@@ -1,4 +1,19 @@
 package com.iadsn.security.jwt;
 
+import com.iadsn.services.ImplDadosUsuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtils {
+
+    @Value("${projeto.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${projeto.jwtExpirationMs}")
+    private String jwtExpirationMs;
+
+    public String generationTokenFromUserDetailsImpl(ImplDadosUsuario userData){
+        return "";
+    }
 }
