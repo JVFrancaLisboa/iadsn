bbc1dbef9329627a5b4f6594b3035e4aaafc016e
JVFrancaLisboa <logiccoder88@gmail.com>
2025-04-28 17:53:39 -0300

    releasing home

--- iadsn/src/main/java/com/iadsn/config/SecurityConfig.java
+++ iadsn/src/main/java/com/iadsn/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/", "/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
